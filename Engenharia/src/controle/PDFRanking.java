48389230f32d7146e09faeb1d12443900598c320
HOME <HOME@HOME-PC>
2017-04-11 02:43:16 -0300

    Organização de partes do código

--- Engenharia/src/controle/PDFRanking.java
+++ Engenharia/src/controle/PDFRanking.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-
 import javax.swing.JOptionPane;
 import java.io.File;
 import com.itextpdf.text.BaseColor;
@@ -19,7 +17,6 @@ import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
-
 import modelo.Jogador;
 
 public class PDFRanking {
